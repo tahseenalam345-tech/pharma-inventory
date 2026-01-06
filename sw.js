d59e9322d0bac47c1fb9b44db3123e418519bd1b
tahseenalam345-tech <tahseenalam345@gmail.com>
2026-01-06 18:30:58 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,23 @@
-self.addEventListener("install", e => {
+const CACHE_NAME = "alazamat-v41";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png",
+  "https://cdn.tailwindcss.com",
+  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css",
+  "https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@300;400;600;800&family=Great+Vibes&display=swap"
+];
+
+self.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open("medical-store").then(cache => {
-      return cache.addAll([
-        "/",
-        "/index.html"
-      ]);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-self.addEventListener("fetch", e => {
+self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+    caches.match(e.request).then((response) => response || fetch(e.request))
   );
 });
